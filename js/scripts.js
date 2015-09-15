c3a447bf93f65debe3edae0764b0c370067bda86
teresa fletcher & kallen millner & colleen minor <student@epicodus.com>
2015-09-15 14:50:27 -0700

    fix bug

--- js/scripts.js
+++ js/scripts.js
@@ -23,35 +23,6 @@ var getWords = function(words){
 
 
 
-  fain—gladly……“I fain would bake Mr. Love cookies if I could get an A.”
-
-  fie—an exclamation of dismay or disgust……“You cheated?  Fie upon it!” OR “Fie!  Are you mad?”
-
-  hark—listen……. “Hark to the owl,” OR “Hark!  The herald angels sing!”
-
-  hence—away…..“Get thee hence, beggar!”  OR “We must hence before the army arrives.”
-
-  hie—hurry……“Hie thee hence, or lose your life!”
-
-  hither—here…..“Come hither, young lad.”
-
-  thither—there……“Look to the east—thither doth the sun arise.”
-
-  hath—has……… “He hath killed many a man.” OR “He hath a horse.”
-
-  ho—hey (roughly equivalent). “Lucius, ho!”  [Brutus calling his servant]
-
-  mark—pay attention to…….. “Mark my words.”
-
-  marry—indeed……“He says I should respond quickly; marry, I want to.”
-
-  pray/prithee—a polite way of asking something……“I prithee answer the question.”
-
-  saucy—cheeky; sassy……“Hence, thou saucy boy!”
-
-  sirrah—a term of address used for inferiors……“Sirrah, bring the letter over here.”
-
-
 
   return words;
 
